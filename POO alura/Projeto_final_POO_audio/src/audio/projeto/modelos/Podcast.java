19b55199cad0c0a80c609aa5aad354cc1fdd5619
HenriqueJCManco <99510373+HenriqueJCManco@users.noreply.github.com>
2024-05-08 10:40:33 -0300

    Projeto final dos conteúdos apresentados
    Foram utilizadas classes, preferi não optar por usar interface para melhorar a construção de classes com Get e Set, além disso posso vir a modificar o projeto alocando interfaces como calcular o tempo que falta para acabar depois da reprodução usando bibliotecas para contabilizar tempo.

---
+++ POO alura/Projeto_final_POO_audio/src/audio/projeto/modelos/Podcast.java
@@ -0,0 +1,32 @@
+package audio.projeto.modelos;
+
+public class Podcast extends Audio{
+    private int numeroDoEpisodio;
+    private String criador;
+    private String  patrocinio;
+
+    public int getNumeroDoEpisodio() {
+        return numeroDoEpisodio;
+    }
+
+    public void setNumeroDoEpisodio(int numeroDoEpisodio) {
+        this.numeroDoEpisodio = numeroDoEpisodio;
+    }
+
+    public String getCriador() {
+        return criador;
+    }
+
+    public void setCriador(String criador) {
+        this.criador = criador;
+    }
+
+    public String getPatrocinio() {
+        return patrocinio;
+    }
+
+    public void setPatrocinio(String patrocinio) {
+        this.patrocinio = patrocinio;
+    }
+
+}
